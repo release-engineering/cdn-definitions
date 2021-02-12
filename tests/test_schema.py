e1a026645b1e7dbc40db421fd658bfb211384007
Caleigh Runge-Hottman <caleigh@redhat.com>
2021-02-12 14:12:17 -0500

    Populate cdn-definitions with various cdn-utils data [RHELDST-4797]

--- tests/test_schema.py
+++ tests/test_schema.py
@@ -45,3 +45,37 @@ def test_relative_path_not_match_schema(data, schema):
     data["rhui_alias"][0]["dest"] = "dest-path"
     with pytest.raises(ValidationError):
         jsonschema.validate(data, schema)
+
+
+def test_invalid_signing_key(data, schema):
+    """Verify that data using an invalid signing key does NOT match the schema."""
+
+    data["signing_keys_mappings"]["platform"]["example_platform"]["ga_keys"] = "INVALID"
+    with pytest.raises(ValidationError):
+        jsonschema.validate(data, schema)
+
+
+@pytest.mark.parametrize(
+    "platform_full_version, platform_version",
+    [("7", "7"), ("5.2", "5.3"), ("7", "5.2"), ("5.2", "7")],
+)
+def test_major_or_minor_regex(data, schema, platform_full_version, platform_version):
+    """Validate the major_or_minor_version regex."""
+
+    # cfme_version_mappings are a major_or_minor_version to major_or_minor_version mapping.
+    data["cfme_version_mappings"][platform_full_version] = platform_version
+    jsonschema.validate(data, schema)
+
+
+@pytest.mark.parametrize(
+    "test_data", ["latest_rhel_6_dist", "latest_rhel6_ga", "current_rhel_dist"]
+)
+def test_current_latest_rhel_string(data, schema, test_data):
+    """Verify that data using invalid env_to_rhel_version_mappings strings do NOT
+    match the schema."""
+
+    data["env_to_releasever_mappings"]["prod"] = {test_data: "3.1"}
+    data["env_to_releasever_mappings"]["stage"] = {test_data: "3.1"}
+    data["env_to_releasever_mappings"]["qa"] = {test_data: "3.1"}
+    with pytest.raises(ValidationError):
+        jsonschema.validate(data, schema)
